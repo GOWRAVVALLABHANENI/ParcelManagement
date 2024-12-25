040884518c7197ead65ac45c4d404ada68802f1c
GOWRAVVALLABHANENI <130890581+GOWRAVVALLABHANENI@users.noreply.github.com>
2024-12-25 11:25:54 +0530

    homepage.js

--- homepage.js
+++ homepage.js
@@ -1,7 +1,15 @@
  // Check if user is logged in, by checking sessionStorage
  const username = sessionStorage.getItem("username");
-
- 
+if (!username) {
+     // If not logged in, redirect to the login page
+     window.location.href = "index.html"; 
+ } else {
+    
+     const usernameDisplay = document.getElementById("usernameDisplay");
+     if (usernameDisplay) {
+         usernameDisplay.textContent = ` ${username}`;
+     }
+ }
 
  // Home link - Redirect to the same page (reload)
  document.getElementById("homeLink").addEventListener("click", function() {
